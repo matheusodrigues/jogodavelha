c0b72c53ef56b10cfc554c6bdb73ffd69cf48251
Matheus <matheus_rlp@outlook.com>
2020-12-08 14:27:18 -0300

    projeto em desenvolvimento

---
+++ app/src/main/java/com/example/atkotlinjogodavelha/atjogodevelha/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.atkotlinjogodavelha.atjogodevelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClick(view: View) {
+        val buttonSelecionado = view as Button
+        var cellID = 0
+
+        when(buttonSelecionado.id) {
+            R.id.button1 -> cellID = 1
+            R.id.button2 -> cellID = 2
+            R.id.button3 -> cellID = 3
+            R.id.button4 -> cellID = 4
+            R.id.button5 -> cellID = 5
+            R.id.button6 -> cellID = 6
+            R.id.button7 -> cellID = 7
+            R.id.button8 -> cellID = 8
+            R.id.button9 -> cellID = 9
+
+        }
+
+        Toast.makeText( context: this, text: "ID: "+cellID, Toast.LENGTH_SHORT).show()
+    }
+
+}
